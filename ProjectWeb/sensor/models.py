98cbc3982d3bbdddae9393d14441ec25658e5828
unknown <ic2140@IC-L-W311A-0040.hh.polyu.hk>
2025-07-07 16:18:04 +0800

    add of mqtt receiver

--- ProjectWeb/sensor/models.py
+++ ProjectWeb/sensor/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Event(models.Model):
+    node_id = models.CharField(max_length=5)
+    loc = models.TextField()
+    temp = models.DecimalField(max_digits=5, decimal_places=2)
+    hum = models.DecimalField(max_digits=5, decimal_places=2)
+    light = models.DecimalField(max_digits=5, decimal_places=2)
+    snd = models.DecimalField(max_digits=5, decimal_places=2)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Event #{self.id}'
--- ProjectWeb/sensor/views.py
+++ ProjectWeb/sensor/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from . import iot_mqtt
+from .models import Event
 # Create your views here.
+
+def index(request):
+    ...
